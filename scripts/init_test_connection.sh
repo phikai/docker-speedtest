28d1d283e24a8a53bb7f14ef0417058e749775fd
Kai Armstrong <kai.armstrong@gmail.com>
2019-05-01 19:23:52 -0500

    changing connection string to non-link dependent name

--- scripts/init_test_connection.sh
+++ scripts/init_test_connection.sh
@@ -10,8 +10,8 @@ do
 	DOWNLOAD=$(cat $FILE | grep "Download:" | awk -F " " '{print $2}')
 	UPLOAD=$(cat $FILE | grep "Upload:" | awk -F " " '{print $2}')
 	echo "Download: $DOWNLOAD Upload: $UPLOAD    $TIMESTAMP"
-	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
-	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
+	curl -i -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
+	curl -i -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
 	sleep $TEST_INTERVAL
 
 done
